894dc79219663f4d0f98cbeee5837c232c09f6e9
Vivi <viviane.souza05@aluno.ifce.edu.br>
2022-06-28 23:10:38 -0300

    update

---
+++ teste1.c
@@ -0,0 +1,13 @@
+
+// iniciar com as bibiliotecas que é quem aloca as funções que nós queremos que estejam presentes para que o propgrama possa funcionar
+#include <stdio.h>
+
+// todo programa em C terá essa "MARCA REGISTRADA", essa assinatura: int main (){}. Isso é o entrypoint, ou seja, onde o programna vai começar
+int main (){
+
+    //saída de dados
+    printf("Ola mundo!");
+
+    // SE CHEGOU ATÉ O FINAL, QUER DIZER QUE NÃO TEVE NENHUM ERRO, ENTÃO POR PADRO, A GENTE COLOCA O RETURN 0; SE TIVER
+    return 0;
+}

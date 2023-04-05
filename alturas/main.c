708f8c01cc0801c06daf14aa04e9e86f113bbc46
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-04-05 19:22:56 -0300

    update

---
+++ alturas/main.c
@@ -0,0 +1,24 @@
+/*Problema "alturas"
+Fazer um programa para ler nome, idade e altura de N pessoas, conforme exemplo. Depois, mostrar na
+tela a altura média das pessoas, e mostrar também a porcentagem de pessoas com menos de 16 anos,
+bem como os nomes dessas pessoas caso houver.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, i;
+
+    printf("Quantas pessoas serao digitadas?");
+    scanf("%d", &n);
+
+    for(i=0;i<n;i++){
+        printf("Dados da ",i+1, "a pessoa:");
+
+
+    }
+
+
+    return 0;
+}

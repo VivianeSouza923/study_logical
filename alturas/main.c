97c94f07905c82b34e088f08c00491e86ebeb653
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-04-13 20:20:29 -0300

    update

--- alturas/main.c
+++ alturas/main.c
@@ -6,18 +6,49 @@ bem como os nomes dessas pessoas caso houver.*/
 #include <stdio.h>
 #include <stdlib.h>
 
+void limpar_entrada() {
+char c;
+while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
 int main()
 {
     int n, i;
+    char nome[n];
+    int idade[n];
+    double altura[n];
+    double contAlt = 0;
+    int contIdade = 0;
+    double altMedia, porc;
 
-    printf("Quantas pessoas serao digitadas?");
+    printf("Quantas pessoas serao digitadas?\n");
     scanf("%d", &n);
 
     for(i=0;i<n;i++){
-        printf("Dados da ",i+1, "a pessoa:");
+        printf("\nDados da %dª pessoa: \n", i+1);
+        limpar_entrada();
+        printf("\nNome: ");
+        scanf("%s", &nome[i]);
+        limpar_entrada();
+        printf("\nIdade: ");
+        scanf("%d", &idade[i]);
+            if(idade[i]<16){
+               contIdade = contIdade + 1;
+            }
+        limpar_entrada();
+        printf("\nAltura: ");
+        scanf("%lf", &altura[i]);
+        contAlt = contAlt + altura[i];
+        }
 
+        altMedia = contAlt/n;
+        printf("Altura média: %lf", altMedia);
 
-    }
+        porc = (contIdade*100)/n;
+        printf("Pessoas com menos de 16 anos: %lf", porc);
+        for(i=0;i<contIdade;i++){
+            printf(nome[i]);
+        }
 
 
     return 0;

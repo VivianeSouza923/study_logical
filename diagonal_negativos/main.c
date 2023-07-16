8a9dc796d7deb4bba8df3503b997f895642f0263
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-07-16 19:04:11 -0300

    today's studies

---
+++ diagonal_negativos/main.c
@@ -0,0 +1,40 @@
+/*Problema "diagonal_negativos"
+Fazer um programa para ler um número inteiro N (máximo = 10) e uma matriz quadrada de ordem N
+contendo números inteiros. Em seguida, mostrar a diagonal principal e a quantidade de valores
+negativos da matriz.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N, i, j;
+
+
+    printf("Qual a ordem da matriz?");
+    scanf("%d", &N);
+
+    int mat[N][N];
+
+    for(i=0; i<N; i++){
+        for(j=0; j<N; j++){
+            printf("Elemento [%d,%d]:", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    printf("DIAGONAL PRINCIPAL: ");
+
+    for(i=0; i<N; i++){
+        for(j=0; j<N; j++){
+            if(i == j){
+                printf("%d ", mat[i][j]);
+
+            }
+
+        }
+    }
+
+
+
+}

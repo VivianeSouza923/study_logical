6892285581c2aa4b5b355058d2f090b9ed6bab24
Viviane Souza <viviane.souza05@aluno.ifce.edu.br>
2024-04-06 18:42:03 -0300

    aplicando lógica de programação com C++

---
+++ retangulo_cpp/main.cpp
@@ -0,0 +1,25 @@
+/*Problema "retangulo"
+Fazer um programa para ler as medidas da base e altura de um
+retângulo. Em seguida, mostrar o valor
+da área, perímetro e diagonal deste retângulo, com quatro
+casas decimais, conforme exemplos.*/
+
+#include <iostream>
+
+using namespace std;
+
+double base, altura, area, perimetro, diagonal;
+
+int main()
+{
+    cout << "Base do retangulo: " << endl;
+    cin >> base;
+
+    cout << "Altura do retangulo: " << endl()
+    cin >> altura;
+
+    area = base * altura;
+    perimetro = 2*base + 2*altura;
+    // TERMINAAAAAR diagonal =
+    return 0;
+}

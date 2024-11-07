edd9b95c7602b77b58214272f072a54104d13f7b
Vivi <70662490+VivianeSouza923@users.noreply.github.com>
2024-11-07 11:11:28 -0300

    adiciona arquivo incompleto da questão retangulo

---
+++ retangulo.java
@@ -0,0 +1,24 @@
+/*
+ * Problema "retangulo"
+Fazer um programa para ler as medidas da base e altura de um retângulo. 
+Em seguida, mostrar o valor
+da área, perímetro e diagonal deste retângulo, com quatro casas decimais, 
+conforme exemplos.
+ */
+package logical_java;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class retangulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+
+	}
+
+}

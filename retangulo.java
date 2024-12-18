46fd213cec92b5eb5dbf1d4977b683ab0115df87
Vivi <70662490+VivianeSouza923@users.noreply.github.com>
2024-12-18 08:33:15 -0300

    exercício básico de java, onde pratico entrada +
    saída e processamento de dados, calculando área, perímetro e diagonal do retângulo.

--- retangulo.java
+++ retangulo.java
@@ -17,6 +17,27 @@ public class retangulo {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		double base, altura, area, perimetro, diagonal;
+		
+		System.out.println("Base do retangulo: ");
+		base = sc.nextDouble();
+		
+		System.out.println("Altura do retangulo: ");
+		altura = sc.nextDouble();
+		
+		area = base * altura;
+		
+		perimetro = 2*base + 2*altura;
+		
+		diagonal = Math.sqrt(Math.pow(base, 2) + Math.pow(altura, 2));
+		
+		
+		System.out.printf("AREA = %.4f%n", area);
+		System.out.printf("PERIMETRO = %.4f%n", perimetro);
+		System.out.printf("DIAGONAL = %.4f%n", diagonal);
+		
+		
+		
 		
 
 	}
